89ba0875ae47a8e7047c12b1342f5edc1fe5951d
ma-awal <maawal797@gmail.com>
2023-02-28 11:18:00 +0600

    final added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+ 
+module.exports = {
+ content: ["./src/**/*.{html,js}",
+ "./dist/**/*.{html,js}"],
+ mode:'jit',
+  theme: {
+     container:{
+      center:true,
+      padding:"16px",
+     },
+
+    extend: {
+    
+    },
+  },
+
+  plugins: [],
+}
